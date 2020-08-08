6fd88650d9c1ef49e4003ac773c1f5e2dac53019
Lakshmi096 <lakshmirk.096@gmail.com>
2020-08-08 21:12:49 +0530

    Updates

---
+++ precache-manifest.cef342eacae9475d22f74c21e9707c9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a66b47a84da5b99460efafd00d2cb09",
+    "url": "/react-spotify-clone/index.html"
+  },
+  {
+    "revision": "6ad054f669b574bb72af",
+    "url": "/react-spotify-clone/static/css/main.ad6ea259.chunk.css"
+  },
+  {
+    "revision": "d36e9227f5f148539df2",
+    "url": "/react-spotify-clone/static/js/2.bf16e024.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-spotify-clone/static/js/2.bf16e024.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad054f669b574bb72af",
+    "url": "/react-spotify-clone/static/js/main.280c5003.chunk.js"
+  },
+  {
+    "revision": "6ff56c6fc989c5692525",
+    "url": "/react-spotify-clone/static/js/runtime-main.fac46db6.js"
+  }
+]);
